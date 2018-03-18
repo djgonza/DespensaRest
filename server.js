28131928e5f282f57897230fdff7d1a7d69a5a0d
djgonza <davidjimenezgonzalez@MacBook-Pro-de-David.local>
2018-03-18 15:30:13 +0100

    Update

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Servidor corriendo en el Puerto: ', port);
 });
